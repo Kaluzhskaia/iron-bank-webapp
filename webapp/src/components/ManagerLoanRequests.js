de8e7726893f7803cc87f8cb7536ba15eb7174dc
Mikhail Falaleev <mf94@bk.ru>
2017-05-04 23:54:00 +0300

    return boring tabs :(

--- webapp/src/components/ManagerLoanRequests.js
+++ webapp/src/components/ManagerLoanRequests.js
@@ -3,12 +3,11 @@
  */
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
-// From https://github.com/oliviertassinari/react-swipeable-views
-import SwipeableViews from 'react-swipeable-views';
 import RequestGrid from './RequestsGrid';
 import {PATH_API_LOAN_REQUEST} from '../constants';
 import {createAuthorizationTokenHeader} from '../utils.js';
 import axios from 'axios';
+
 
 const styles = {
     headline: {
@@ -51,55 +50,28 @@ const ManagerLoanRequests = React.createClass({
 
         return {
             requestsList: [],
-            userInfo: {
-                slideIndex: 0,
-            },
         }
 
     },
 
-    handleChange(value){
-        this.setState({
-            slideIndex: value,
-        });
-    },
 
-    isNew(loanRequest) {
-    return loanRequest.loanRequestStatus === "NOT_REVIEWED";
-    },
-
-    isA(loanRequest) {
-        return loanRequest.loanRequestStatus === "NOT_REVIEWED";
-    },
     render() {
         return (
             <div>
-                <Tabs
-                    onChange={this.handleChange}
-                    value={this.state.slideIndex}
-                >
-                    <Tab label="Новые" value={0} />
-                    <Tab label="Одобренные" value={1} />
-                    <Tab label="Отклоненные" value={2} />
-                    <Tab label="Все" value={3} />
-                </Tabs>
-                <SwipeableViews
-                    index={this.state.slideIndex}
-                    onChangeIndex={this.handleChange}
-                >
-                    <div>
+                <Tabs>
+                    <Tab label="Новые" >
                         <RequestGrid shouldUpdate={this.shouldUpdate} requestsList={this.state.requestsList.filter((req)=>req.loanRequestStatus === "NOT_REVIEWED")}/>
-                    </div>
-                    <div>
+                    </Tab>
+                    <Tab label="Одобренные"  >
                         <RequestGrid shouldUpdate={this.shouldUpdate} requestsList={this.state.requestsList.filter((req)=>req.loanRequestStatus === "APPROVED")}/>
-                    </div>
-                    <div>
+                    </Tab>
+                    <Tab label="Отклоненные"  >
                         <RequestGrid shouldUpdate={this.shouldUpdate} requestsList={this.state.requestsList.filter((req)=>req.loanRequestStatus === "REJECTED")}/>
-                    </div>
-                    <div>
+                    </Tab>
+                    <Tab label="Все" value={3} >
                         <RequestGrid shouldUpdate={this.shouldUpdate} requestsList={this.state.requestsList}/>
-                    </div>
-                </SwipeableViews>
+                    </Tab>
+                </Tabs>
             </div>
         )
     }
--- webapp/src/components/RequestsGrid.js
+++ webapp/src/components/RequestsGrid.js
@@ -7,8 +7,8 @@ import LoanRequest from './LoanRequest.js'
 const RequestGrid = React.createClass({
 
     render: function () {
-        const requestList = this.props.requestsList.map(loanRequest =>
-            <LoanRequest shouldUpdate={this.props.shouldUpdate} loanRequest={loanRequest} key={loanRequest.id}/>);
+        const requestList = this.props.requestsList.length > 0 ? this.props.requestsList.map(loanRequest =>
+            <LoanRequest shouldUpdate={this.props.shouldUpdate} loanRequest={loanRequest} key={loanRequest.id}/>) : <h2>No requests</h2>;
         return(
             <div>
                 {requestList}
